ba3ee6d231748e5bae55916abb75a3a99b1d4072
Viktoria Teneva <teneva.victoria@gmail.com>
2019-11-03 00:54:08 +0200

    Change destination dir name and add constant for data chunks

--- projects/deep-learning-for-audio-classification/downsample_remove_noise.py
+++ projects/deep-learning-for-audio-classification/downsample_remove_noise.py
@@ -8,14 +8,15 @@ from scipy.io import wavfile
 from utils import envelope
 
 DATA_DIR = r'../../data/freesound-audio-tagging/audio_train'
-DEST_DIR = r'samples'
+DEST_DIR = r'clean'
 NOISE_AMPLITUDE_THRESHOLD = 0.005
 DOWNSAMPLED_SAMPLE_RATE = 16000
+DATA_CHUNKS_PER_SECOND = 10
 
 
 def process_signal(filename):
     signal, sample_rate = librosa.load(p.join(DATA_DIR, filename), sr=DOWNSAMPLED_SAMPLE_RATE)
-    rolling_mean_signal = envelope(signal, sample_rate)
+    rolling_mean_signal = envelope(signal, sample_rate, DATA_CHUNKS_PER_SECOND)
     noise_mask = rolling_mean_signal > NOISE_AMPLITUDE_THRESHOLD
     wavfile.write(filename=p.join(DEST_DIR, filename),
                   rate=DOWNSAMPLED_SAMPLE_RATE,
