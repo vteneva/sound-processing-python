d39c9bf02ec5438a0f4c51ba2410963a71a7a429
Viktoria Teneva <teneva.victoria@gmail.com>
2019-11-02 12:00:20 +0200

    Downsampling and noise removal script

---
+++ projects/deep-learning-for-audio-classification/downsample_remove_noise.py
@@ -0,0 +1,29 @@
+import os
+import os.path as p
+
+import librosa
+import pandas as pd
+from scipy.io import wavfile
+
+from utils import envelope
+
+DATA_DIR = r'../../data/freesound-audio-tagging/audio_train'
+DEST_DIR = r'samples'
+NOISE_AMPLITUDE_THRESHOLD = 0.005
+DOWNSAMPLED_SAMPLE_RATE = 16000
+
+
+def process_signal(filename):
+    signal, sample_rate = librosa.load(p.join(DATA_DIR, filename), sr=DOWNSAMPLED_SAMPLE_RATE)
+    rolling_mean_signal = envelope(signal, sample_rate)
+    noise_mask = rolling_mean_signal > NOISE_AMPLITUDE_THRESHOLD
+    wavfile.write(filename=p.join(DEST_DIR, filename),
+                  rate=DOWNSAMPLED_SAMPLE_RATE,
+                  data=signal[noise_mask])
+
+
+if __name__ == '__main__':
+    audio_data = pd.read_csv('instruments.csv')
+
+    os.makedirs(DEST_DIR)
+    audio_data.fname.apply(process_signal)
