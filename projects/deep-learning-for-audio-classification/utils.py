c8b71dc29db76b9b3e68873c998474f42d843637
Viktoria Teneva <teneva.victoria@gmail.com>
2019-11-02 11:59:54 +0200

    Add envelope function

--- projects/deep-learning-for-audio-classification/utils.py
+++ projects/deep-learning-for-audio-classification/utils.py
@@ -1,6 +1,17 @@
+import numpy as np
+import pandas as pd
 from IPython.display import display, Audio
 
 
 def play_wave(signal, rate):
     audio = Audio(data=signal, rate=rate)
     return display(audio)
+
+
+def envelope(signal, sample_rate):
+    window_size = int(sample_rate / 10)
+
+    rolling_mean_amplitude = (pd.Series(np.abs(signal))
+                              .rolling(window=window_size, min_periods=1, center=True)
+                              .mean())
+    return rolling_mean_amplitude
